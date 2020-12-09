b6daa05cead8d237978aba0586d430c77b876877
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-12-09 19:34:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-export default function appScr(express, bodyParser, fs, crypto, http, CORS, User, m) {
+export default function appScr(express, bodyParser, createReadStream, crypto, http, CORS, writeFileSync, User, UserController, LOGIN, puppeteer, unlinkSync, fs, m) {
     const app = express();
     const path = import.meta.url.substring(7);
     const headersHTML = {'Content-Type':'text/html; charset=utf-8',...CORS}
