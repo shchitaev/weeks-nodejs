6d096b4c8981d993798379d7d3be4867558260e2
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-12-11 00:01:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
 
     app 
     
-    .use(express.static('./public'))
     .use(bodyParser.urlencoded({ extended: true }))
     .use(bodyParser.json())
     
@@ -25,7 +24,21 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
               });           
         })
         .all('/wordpress1/', r => {
-            r.res.set(headersTEXT).send('wordpress1')
+                    const { URL } = 'https://week8.kodaktor.ru/wordpress/';
+        const browser = await puppeteer.launch({
+                args: [
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox',
+                      ],
+                        });
+        const page = await browser.newPage();
+        console.log(URL);
+        await page.goto(URL);
+        await page.waitForSelector('#bt');
+        await page.click('#bt'); 
+        await page.waitForSelector('#inp');
+        alert('ок')
+        browser.close();
         })      
         .all('/sha1/:input/', r => {
             r.res.set(headersTEXT).send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
